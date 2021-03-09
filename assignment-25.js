64c649187da60125e1cd44742abde28e138e02c2
griff-g <gould.griffyn@gmail.com>
2021-03-09 14:16:02 -0500

    adding assignment 25 and 26

--- assignment-25.js
+++ assignment-25.js
@@ -1,1 +1,6 @@
 //Create a function that takes the age and return the age in days.
+const age= function (age) {
+  return age * 365;
+};
+const ageDays = age(15);
+console.log(ageDays);
--- assignment-26.js
+++ assignment-26.js
@@ -1,1 +1,6 @@
 // Create a function that takes length and width and finds the perimeter of a rectangle.
+const calcPerimeter = function(length,width){
+    return 2*(length+width);
+}
+const valuePerimeter = calcPerimeter(20,15);
+console.log(valuePerimeter);
