63a8c70ce732beeb3c50b2b9e21acefd50b80011
ronin175 <pratikghagare1797@gmail.com>
2021-03-10 23:50:22 +0530

    added assignment 27 missing

--- assignment-27.js
+++ assignment-27.js
@@ -1,12 +1,16 @@
 //Create a function that finds the maximum range of a triangle's third edge,
 //where the side lengths are all integers.
 
-//all should be positive integers/number
-
 //(side1 + side2) - 1 = maximum range of third edge.
 
-function nextEdge(side1, side2) {}
+function nextEdge(side1, side2) {
+  if (side1 > 0 && side2 > 0) {
+    return side1 + side2 - 1;
+  } else {
+    return -1;
+  }
+}
 
-nextEdge(8, 10);
+console.log(nextEdge(8, 10));
 
 //Answer = 17
